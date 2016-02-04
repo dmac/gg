63617b547af34e268dbbf5795079e898a7e3e8c9
Daniel MacDougall <dmacdougall@gmail.com>
2016-02-04 00:31:12 -0800

    Remove old examples

--- examples/tetris/tetris.go
+++ examples/tetris/tetris.go
@@ -1,254 +1,100 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 
-	"github.com/dmac/gg/v2.1/gg"
+	"github.com/dmac/gg"
+	mgl "github.com/go-gl/mathgl/mgl32"
+)
+
+const (
+	Padding      = 100
+	CellSize     = 16
+	WidthCells   = 10
+	HeightCells  = 14
+	WindowWidth  = 2*Padding + CellSize*WidthCells
+	WindowHeight = 2*Padding + CellSize*HeightCells
 )
 
 type Tetris struct {
-	bg       *gg.Poly
+	textures map[string]*gg.Texture
+	bg       *Sprite
 	board    *Board
-	gameOver bool
-	ticker   *time.Ticker
 	score    int
+
+	mu sync.Mutex
+	gameOver bool
 }
 
-func NewTetris(windowWidth, windowHeight int) *Tetris {
-	bg := gg.NewPoly([][2]float32{
-		{0, 0},
-		{0, float32(windowHeight)},
-		{float32(windowWidth), float32(windowHeight)},
-		{float32(windowWidth), 0},
-	})
-	bg.SetColor(0.3, 0.2, 0.3, 1)
-	board := NewBoard(10, 24)
-	board.bg.Position = [2]float32{100, 50}
+func NewTetris(vertShader, fragShader string) (*Tetris, error) {
+	gg.Enable(gg.BLEND)
+	gg.BlendFunc(gg.SRC_ALPHA, gg.ONE_MINUS_SRC_ALPHA)
 
-	t := &Tetris{
-		bg:     bg,
-		board:  board,
-		ticker: time.NewTicker(time.Second),
+	// Compile the global shader program
+	vshader, err := gg.CreateShader([]byte(vertShader), gg.VERTEX_SHADER)
+	if err != nil {
+		return nil, err
+	}
+	fshader, err := gg.CreateShader([]byte(fragShader), gg.FRAGMENT_SHADER)
+	if err != nil {
+		return nil, err
+	}
+	program := gg.CreateProgram()
+	gg.AttachShader(program, vshader)
+	gg.AttachShader(program, fshader)
+	if err := gg.LinkProgram(program); err != nil {
+		return nil, err
 	}
 
+	// Set the global projection matrix
+	gg.UseProgram(program)
+	proj := mgl.Ortho(0, float32(WindowWidth), float32(WindowHeight), 0, 0, 1)
+	projUniform, err := gg.GetUniformLocation(program, "proj")
+	if err != nil {
+		return nil, err
+	}
+	gg.UniformMatrix4fv(projUniform, proj[:])
+
+	tetris := &Tetris{}
+
+	tetris.textures, err = LoadTextures()
+	if err != nil {
+		return nil, err
+	}
+
+	tetris.bg = NewSprite(WindowWidth, WindowHeight, program, tetris.textures["bg"])
+	tetris.board = NewBoard(WidthCells, HeightCells, program, tetris.textures)
+	tetris.board.x = Padding
+	tetris.board.y = Padding
+
+	rand.Seed(time.Now().Unix())
+	tetris.board.current = tetris.board.NewRandomPiece()
+
 	go func() {
-		for range time.NewTicker(time.Second).C {
-			t.HandleInput(inputDown)
+		t := time.NewTicker(time.Second)
+		for range t.C {
+			tetris.mu.Lock()
+			if tetris.gameOver {
+				return
+			}
+			tetris.mu.Unlock()
+			tetris.HandleInput(inputDown)
 		}
 	}()
 
-	return t
+	return tetris, nil
 }
 
 func (t *Tetris) Draw() {
+	gg.ClearColor(0.5, 0.5, 0.5, 1.0)
+	gg.Clear(gg.COLOR_BUFFER_BIT | gg.DEPTH_BUFFER_BIT)
 	t.bg.Draw()
 	t.board.Draw()
-}
-
-func (t *Tetris) HandleInput(input Input) {
-	if t.gameOver {
-		return
-	}
-	current := t.board.current
-	movedPiece := current.Copy()
-	switch input {
-	case inputUp:
-		movedPiece.orientation = (movedPiece.orientation + 1) % len(orientations[movedPiece.typ])
-	case inputDown:
-		movedPiece.row += 1
-	case inputLeft:
-		movedPiece.col -= 1
-	case inputRight:
-		movedPiece.col += 1
-	}
-
-	if movedPiece.Valid() {
-		t.board.current = movedPiece
-		return
-	}
-	if input == inputDown {
-		t.board.AnchorCurrent()
-		cleared := t.board.ClearLines()
-		if cleared > 0 {
-			t.score += cleared
-			fmt.Println("Score:", t.score)
-		}
-		t.board.current = NewPiece(t.board)
-		if !t.board.current.Valid() {
-			fmt.Println("Game over!")
-			t.gameOver = true
-		}
-	}
-}
-
-type Board struct {
-	bg      *gg.Poly
-	sprs    map[string]*gg.Sprite
-	grid    [][]string
-	current *Piece
-}
-
-const cellsize = 16
-
-func NewBoard(width, height int) *Board {
-	bg := gg.NewPoly([][2]float32{
-		{0, 0},
-		{0, float32(height * cellsize)},
-		{float32(width * cellsize), float32(height * cellsize)},
-		{float32(width * cellsize), 0},
-	})
-	bg.SetColor(0, 0, 0, 1)
-
-	grid := make([][]string, height)
-	for i := 0; i < height; i++ {
-		grid[i] = make([]string, width)
-	}
-
-	board := &Board{
-		bg:   bg,
-		grid: grid,
-		sprs: make(map[string]*gg.Sprite),
-	}
-	board.current = NewPiece(board)
-	board.sprs["I"] = gg.NewSpriteFromTexture(textures["cyan"])
-	board.sprs["J"] = gg.NewSpriteFromTexture(textures["blue"])
-	board.sprs["L"] = gg.NewSpriteFromTexture(textures["orange"])
-	board.sprs["S"] = gg.NewSpriteFromTexture(textures["green"])
-	board.sprs["Z"] = gg.NewSpriteFromTexture(textures["red"])
-	board.sprs["O"] = gg.NewSpriteFromTexture(textures["yellow"])
-	board.sprs["T"] = gg.NewSpriteFromTexture(textures["purple"])
-
-	return board
-}
-
-func (b *Board) AnchorCurrent() {
-	cells := orientations[b.current.typ][b.current.orientation]
-	for row, cols := range cells {
-		for col, cell := range cols {
-			if cell {
-				b.current.board.grid[b.current.row+row][b.current.col+col] = b.current.typ
-			}
-		}
-	}
-}
-
-func (b *Board) ClearLines() (cleared int) {
-	for row := len(b.grid) - 1; row >= 0; {
-		full := true
-		for _, cell := range b.grid[row] {
-			if cell == "" {
-				full = false
-				break
-			}
-		}
-		if !full {
-			row--
-			continue
-		}
-		cleared++
-		for r := row; r > 0; r-- {
-			for c := 0; c < len(b.grid[r]); c++ {
-				b.grid[r][c] = b.grid[r-1][c]
-			}
-		}
-		for c := 0; c < len(b.grid[0]); c++ {
-			b.grid[0][c] = ""
-		}
-	}
-	return
-}
-
-func (b *Board) Draw() {
-	b.bg.Draw()
-	b.current.Draw()
-
-	for row, cols := range b.grid {
-		for col, cell := range cols {
-			if cell != "" {
-				spr := b.sprs[b.grid[row][col]]
-				spr.SetPosition(
-					b.bg.Position[0]+float32(cellsize*col),
-					b.bg.Position[1]+float32(cellsize*row),
-				)
-				spr.Draw()
-			}
-		}
-	}
-}
-
-type Piece struct {
-	board       *Board
-	row, col    int
-	typ         string
-	orientation int
-}
-
-func NewPiece(board *Board) *Piece {
-	var typs []string
-	for typ := range orientations {
-		typs = append(typs, typ)
-	}
-
-	return &Piece{
-		row:         1,
-		col:         2,
-		board:       board,
-		typ:         typs[rand.Intn(len(typs))],
-		orientation: 0,
-	}
-}
-
-func (p *Piece) Draw() {
-	boardPosition := p.board.bg.Position
-	cells := orientations[p.typ][p.orientation]
-	for row, cols := range cells {
-		for col, cell := range cols {
-			if cell {
-				spr := p.board.sprs[p.typ]
-				spr.SetPosition(
-					boardPosition[0]+float32(cellsize*(p.col+col)),
-					boardPosition[1]+float32(cellsize*(p.row+row)),
-				)
-				spr.Draw()
-			}
-		}
-	}
-}
-
-func (p *Piece) Valid() bool {
-	if p.row < 0 {
-		return false
-	}
-	if p.col < 0 {
-		return false
-	}
-	cells := orientations[p.typ][p.orientation]
-	if p.col+len(cells[0]) > len(p.board.grid[0]) {
-		return false
-	}
-	if p.row+len(cells) > len(p.board.grid) {
-		return false
-	}
-	for row, cols := range cells {
-		for col, cell := range cols {
-			if cell && p.board.grid[p.row+row][p.col+col] != "" {
-				return false
-			}
-		}
-	}
-	return true
-}
-
-func (p *Piece) Copy() *Piece {
-	return &Piece{
-		board:       p.board,
-		row:         p.row,
-		col:         p.col,
-		typ:         p.typ,
-		orientation: p.orientation,
-	}
 }
 
 type Input byte
@@ -261,27 +107,352 @@ const (
 	inputSpace
 )
 
-func (i Input) String() string {
-	switch i {
+func (t *Tetris) HandleInput(input Input) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	if t.gameOver {
+		return
+	}
+	movedPiece := t.board.current.Copy()
+	switch input {
 	case inputUp:
-		return "up"
-	case inputRight:
-		return "right"
+		movedPiece.orientation = (movedPiece.orientation + 1) % len(orientations[movedPiece.kind])
 	case inputDown:
-		return "down"
+		movedPiece.row += 1
 	case inputLeft:
-		return "left"
+		movedPiece.col -= 1
+	case inputRight:
+		movedPiece.col += 1
 	case inputSpace:
-		return "space"
-	default:
-		return "unknown"
+		for movedPiece.Valid() {
+			t.board.current = movedPiece.Copy()
+			movedPiece.row += 1
+		}
+	}
+	if movedPiece.Valid() {
+		t.board.current = movedPiece
+		return
+	}
+	if input != inputDown && input != inputSpace {
+		return
+	}
+	fmt.Printf("%#v\n", t.board.current)
+	t.board.AnchorCurrent()
+	cleared := t.board.ClearLines()
+	if cleared > 0 {
+		t.score += cleared
+		fmt.Println("Score:", t.score)
+	}
+	t.board.current = t.board.NewRandomPiece()
+	if !t.board.current.Valid() {
+		fmt.Println("Game over!")
+		t.gameOver = true
 	}
 }
 
+type Color byte
+
+const (
+	NoColor Color = iota
+	Red
+	Orange
+	Yellow
+	Green
+	Blue
+	Cyan
+	Purple
+)
+
+type Board struct {
+	x, y          float32
+	width, height int
+	bg            *Sprite
+	blockSprites  map[Color]*Sprite
+	cells         [][]Color
+	current       *Piece
+}
+
+func NewBoard(width, height int, program *gg.Program, textures map[string]*gg.Texture) *Board {
+	const boardOriginX, boardOriginY = CellSize * 4, CellSize * 4
+	board := &Board{
+		x:      boardOriginX,
+		y:      boardOriginY,
+		width:  width,
+		height: height,
+	}
+	board.bg = NewSprite(float32(width)*CellSize, float32(height)*CellSize, program, textures["board"])
+	board.cells = make([][]Color, board.height)
+	for row := 0; row < board.height; row++ {
+		board.cells[row] = make([]Color, board.width)
+	}
+
+	board.blockSprites = make(map[Color]*Sprite)
+	board.blockSprites[Red] = NewSprite(CellSize, CellSize, program, textures["red"])
+	board.blockSprites[Orange] = NewSprite(CellSize, CellSize, program, textures["orange"])
+	board.blockSprites[Yellow] = NewSprite(CellSize, CellSize, program, textures["yellow"])
+	board.blockSprites[Green] = NewSprite(CellSize, CellSize, program, textures["green"])
+	board.blockSprites[Blue] = NewSprite(CellSize, CellSize, program, textures["blue"])
+	board.blockSprites[Cyan] = NewSprite(CellSize, CellSize, program, textures["cyan"])
+	board.blockSprites[Purple] = NewSprite(CellSize, CellSize, program, textures["purple"])
+
+	return board
+}
+
+func (b *Board) Draw() {
+	b.bg.x = b.x
+	b.bg.y = b.y
+	b.bg.Draw()
+	b.current.Draw()
+	for row := 0; row < b.height; row++ {
+		for col := 0; col < b.width; col++ {
+			spr, ok := b.blockSprites[b.cells[row][col]]
+			if !ok {
+				continue
+			}
+			spr.x = b.x + CellSize*float32(col)
+			spr.y = b.y + CellSize*float32(row)
+			spr.Draw()
+		}
+	}
+}
+
+type PieceKind byte
+
+const (
+	I PieceKind = iota
+	O
+	T
+	S
+	Z
+	L
+	J
+)
+
+type Piece struct {
+	board       *Board
+	kind        PieceKind
+	row, col    int
+	orientation int
+}
+
+func (b *Board) NewPiece(kind PieceKind, row, col int) *Piece {
+	return &Piece{
+		board: b,
+		kind:  kind,
+		row:   row,
+		col:   col,
+	}
+}
+
+func (b *Board) NewRandomPiece() *Piece {
+	kind := PieceKind(rand.Intn(7))
+	return b.NewPiece(kind, 0, len(b.cells[0])/2)
+}
+
+func (b *Board) ClearLines() (cleared int) {
+	for row := len(b.cells) - 1; row >= 0; {
+		full := true
+		for col := 0; col < len(b.cells[0]); col++ {
+			if b.cells[row][col] == NoColor {
+				full = false
+				break
+			}
+		}
+		if !full {
+			row--
+			continue
+		}
+		cleared++
+		for r := row; r > 0; r-- {
+			for c := 0; c < len(b.cells[0]); c++ {
+				b.cells[r][c] = b.cells[r-1][c]
+			}
+		}
+		for c := 0; c < len(b.cells[0]); c++ {
+			b.cells[0][c] = NoColor
+		}
+	}
+	return cleared
+}
+
+func (b *Board) AnchorCurrent() {
+	p := b.current
+	cells := orientations[p.kind][p.orientation]
+	for row := 0; row < len(cells); row++ {
+		for col := 0; col < len(cells[0]); col++ {
+			if cells[row][col] {
+				b.cells[p.row+row][p.col+col] = p.Color()
+			}
+		}
+	}
+}
+
+func (p *Piece) Draw() {
+	if p == nil {
+		return
+	}
+	spr := p.board.blockSprites[p.Color()]
+	cells := orientations[p.kind][p.orientation]
+	for row := 0; row < len(cells); row++ {
+		for col := 0; col < len(cells[0]); col++ {
+			if !cells[row][col] {
+				continue
+			}
+			spr.x = p.board.x + float32(p.col+col)*CellSize
+			spr.y = p.board.y + float32(p.row+row)*CellSize
+			spr.Draw()
+		}
+	}
+}
+
+func (p *Piece) Color() Color {
+	switch p.kind {
+	case I:
+		return Cyan
+	case O:
+		return Yellow
+	case T:
+		return Purple
+	case S:
+		return Green
+	case Z:
+		return Red
+	case L:
+		return Orange
+	case J:
+		return Blue
+	}
+	return NoColor
+}
+
+func (p *Piece) Copy() *Piece {
+	tmp := *p
+	return &tmp
+}
+
+func (p *Piece) Valid() bool {
+	if p.row < 0 || p.col < 0 {
+		return false
+	}
+	cells := orientations[p.kind][p.orientation]
+	if p.row+len(cells) > len(p.board.cells) {
+		return false
+	}
+	if p.col+len(cells[0]) > len(p.board.cells[0]) {
+		return false
+	}
+	for row := 0; row < len(cells); row++ {
+		for col := 0; col < len(cells[0]); col++ {
+			if cells[row][col] && p.board.cells[p.row+row][p.col+col] != NoColor {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+type Sprite struct {
+	x, y     float32
+	rotation float32
+	scale    float32
+	width    float32
+	height   float32
+
+	program *gg.Program
+	pbuf    *gg.Buffer
+	tbuf    *gg.Buffer
+	tex     *gg.Texture
+}
+
+func NewSprite(width, height float32, program *gg.Program, texture *gg.Texture) *Sprite {
+	s := &Sprite{
+		scale:   1,
+		width:   width,
+		height:  height,
+		tex:     texture,
+		program: program,
+	}
+
+	pvertices := []float32{
+		0, 0, 0,
+		0, s.height, 0,
+		s.width, s.height, 0,
+		s.width, 0, 0,
+	}
+	buf := new(bytes.Buffer)
+	if err := binary.Write(buf, binary.LittleEndian, pvertices); err != nil {
+		panic(err)
+	}
+	s.pbuf = gg.CreateBuffer()
+	gg.BindBuffer(gg.ARRAY_BUFFER, s.pbuf)
+	gg.BufferData(gg.ARRAY_BUFFER, buf.Bytes(), gg.STATIC_DRAW)
+
+	tvertices := []float32{
+		0, 0,
+		0, 1,
+		1, 1,
+		1, 0,
+	}
+	buf.Reset()
+	if err := binary.Write(buf, binary.LittleEndian, tvertices); err != nil {
+		panic(err)
+	}
+	s.tbuf = gg.CreateBuffer()
+	gg.BindBuffer(gg.ARRAY_BUFFER, s.tbuf)
+	gg.BufferData(gg.ARRAY_BUFFER, buf.Bytes(), gg.STATIC_DRAW)
+	return s
+}
+
+func (s *Sprite) Draw() error {
+	gg.UseProgram(s.program)
+
+	model := s.transform()
+	modelUniform, err := gg.GetUniformLocation(s.program, "model")
+	if err != nil {
+		return err
+	}
+	gg.UniformMatrix4fv(modelUniform, model[:])
+
+	gg.ActiveTexture(gg.TEXTURE0)
+	gg.BindTexture(gg.TEXTURE_2D, s.tex)
+	textureUniform, err := gg.GetUniformLocation(s.program, "tex_loc")
+	if err != nil {
+		return err
+	}
+	gg.Uniform1i(textureUniform, 0)
+
+	vattrib, err := gg.GetAttribLocation(s.program, "vertex_position")
+	if err != nil {
+		return err
+	}
+	gg.EnableVertexAttribArray(vattrib)
+	gg.BindBuffer(gg.ARRAY_BUFFER, s.pbuf)
+	gg.VertexAttribPointer(vattrib, 3, gg.FLOAT, false, 0, 0)
+
+	tattrib, err := gg.GetAttribLocation(s.program, "vertex_texture")
+	if err != nil {
+		return err
+	}
+	gg.EnableVertexAttribArray(tattrib)
+	gg.BindBuffer(gg.ARRAY_BUFFER, s.tbuf)
+	gg.VertexAttribPointer(tattrib, 2, gg.FLOAT, false, 0, 0)
+
+	gg.DrawArrays(gg.TRIANGLE_FAN, 0, 4)
+	return nil
+}
+
+func (s *Sprite) transform() mgl.Mat4 {
+	S := mgl.Scale2D(s.scale, s.scale).Mat4()
+	R := mgl.Rotate2D(mgl.DegToRad(s.rotation)).Mat4()
+	T := mgl.Translate3D(s.x, s.y, 0)
+	return T.Mul4(R).Mul4(S)
+}
+
+//TODO(dmac) Replace hard-coded rotations with real matrix rotation
 type Orientation [][]bool
 
-var orientations = map[string][]Orientation{
-	"L": {
+var orientations = map[PieceKind][]Orientation{
+	L: {
 		{
 			{true, false, false},
 			{true, true, true},
@@ -301,7 +472,7 @@ var orientations = map[string][]Orientation{
 			{true, true},
 		},
 	},
-	"J": {
+	J: {
 		{
 			{false, false, true},
 			{true, true, true},
@@ -321,13 +492,13 @@ var orientations = map[string][]Orientation{
 			{false, true},
 		},
 	},
-	"O": {
+	O: {
 		{
 			{true, true},
 			{true, true},
 		},
 	},
-	"I": {
+	I: {
 		{
 			{true, true, true, true},
 		},
@@ -338,7 +509,7 @@ var orientations = map[string][]Orientation{
 			{true},
 		},
 	},
-	"S": {
+	S: {
 		{
 			{false, true, true},
 			{true, true, false},
@@ -349,7 +520,7 @@ var orientations = map[string][]Orientation{
 			{false, true},
 		},
 	},
-	"Z": {
+	Z: {
 		{
 			{true, true, false},
 			{false, true, true},
@@ -360,7 +531,7 @@ var orientations = map[string][]Orientation{
 			{true, false},
 		},
 	},
-	"T": {
+	T: {
 		{
 			{false, true, false},
 			{true, true, true},
--- examples/tetris/tetris.native.go
+++ examples/tetris/tetris.native.go
@@ -3,20 +3,17 @@
 package main
 
 import (
-	"fmt"
-	"image"
 	"image/png"
 	"log"
 	"os"
+	"path/filepath"
 	"runtime"
 
-	"github.com/dmac/gg/v2.1/gg"
+	"github.com/dmac/gg"
+	_ "github.com/dmac/gg/v2.1/gg"
 	"github.com/go-gl/gl/v2.1/gl"
 	"github.com/go-gl/glfw/v3.1/glfw"
 )
-
-const WindowWidth = 640
-const WindowHeight = 480
 
 func main() {
 	runtime.LockOSThread()
@@ -25,36 +22,48 @@ func main() {
 	}
 	defer glfw.Terminate()
 
-	//glfw.WindowHint(glfw.ContextVersionMajor, 3)
-	//glfw.WindowHint(glfw.ContextVersionMinor, 2)
-	//glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
-	//glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
-
 	if err := gl.Init(); err != nil {
 		log.Fatal(err)
 	}
+
 	window, err := glfw.CreateWindow(WindowWidth, WindowHeight, "Tetris", nil, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 	window.MakeContextCurrent()
 
-	if err := gg.Init(WindowWidth, WindowHeight); err != nil {
+	const vshader = `#version 120
+
+uniform mat4 proj, model;
+attribute vec3 vertex_position;
+attribute vec2 vertex_texture;
+varying vec2 texture_coordinates;
+
+void main() {
+	gl_Position = proj * model * vec4(vertex_position, 1);
+	texture_coordinates = vertex_texture;
+}
+`
+
+	const fshader = `#version 120
+
+uniform sampler2D tex_loc;
+varying vec2 texture_coordinates;
+
+void main() {
+	gl_FragColor = texture2D(tex_loc, texture_coordinates);
+}
+`
+
+	tetris, err := NewTetris(vshader, fshader)
+	if err != nil {
 		log.Fatal(err)
 	}
-
-	loadTextures()
-
-	tetris := NewTetris(WindowWidth, WindowHeight)
 
 	window.SetKeyCallback(tetris.handleKeyInput)
 
 	for !window.ShouldClose() {
-		gl.ClearColor(0.5, 0.5, 0.5, 1.0)
-		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-
 		tetris.Draw()
-
 		glfw.PollEvents()
 		window.SwapBuffers()
 	}
@@ -73,27 +82,67 @@ func (t *Tetris) handleKeyInput(w *glfw.Window, key glfw.Key, scancode int, acti
 		t.HandleInput(inputDown)
 	case glfw.KeyA:
 		t.HandleInput(inputLeft)
+	case glfw.KeySpace:
+		t.HandleInput(inputSpace)
 	}
 }
 
-var textures map[string]*gg.Texture
-
-func loadTextures() {
-	textures = make(map[string]*gg.Texture)
-	for _, color := range []string{"blue", "cyan", "green", "orange", "purple", "red", "yellow"} {
-		img, err := openPNG(color + ".png")
+func LoadTextures() (map[string]*gg.Texture, error) {
+	textures := make(map[string]*gg.Texture)
+	for _, name := range []string{
+		"bg", "board",
+		"red", "orange", "yellow", "green", "blue", "cyan", "purple",
+	} {
+		t, err := newImageTexture(filepath.Join("images", name+".png"))
 		if err != nil {
-			fmt.Println(err)
-			continue
+			return nil, err
 		}
-		textures[color] = gg.NewTextureFromImage(img)
+		textures[name] = t
 	}
+	return textures, nil
 }
 
-func openPNG(filename string) (image.Image, error) {
+// TODO(dmac) Move into gg helpers package
+func newImageTexture(filename string) (*gg.Texture, error) {
 	f, err := os.Open(filename)
 	if err != nil {
 		return nil, err
 	}
-	return png.Decode(f)
+	img, err := png.Decode(f)
+	if err != nil {
+		return nil, err
+	}
+
+	var buf []byte
+	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
+		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
+			r, g, b, a := img.At(x, y).RGBA()
+			buf = append(buf, byte(r/256))
+			buf = append(buf, byte(g/256))
+			buf = append(buf, byte(b/256))
+			buf = append(buf, byte(a/256))
+		}
+	}
+	tex := gg.CreateTexture()
+	gg.ActiveTexture(gg.TEXTURE0)
+	gg.Enable(gg.TEXTURE_2D)
+	gg.BindTexture(gg.TEXTURE_2D, tex)
+	gg.TexImage2D(
+		gg.TEXTURE_2D,
+		0,
+		gg.RGBA,
+		img.Bounds().Dx(),
+		img.Bounds().Dy(),
+		0,
+		gg.RGBA,
+		gg.UNSIGNED_BYTE,
+		buf,
+	)
+
+	gg.TexParameteri(gg.TEXTURE_2D, gg.TEXTURE_WRAP_S, gg.CLAMP_TO_EDGE)
+	gg.TexParameteri(gg.TEXTURE_2D, gg.TEXTURE_WRAP_T, gg.CLAMP_TO_EDGE)
+	gg.TexParameteri(gg.TEXTURE_2D, gg.TEXTURE_MAG_FILTER, gg.LINEAR)
+	gg.TexParameteri(gg.TEXTURE_2D, gg.TEXTURE_MIN_FILTER, gg.LINEAR)
+
+	return tex, nil
 }
